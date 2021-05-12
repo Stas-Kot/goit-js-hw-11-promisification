1b82494f444a26ae3cefd4dcf59458dfa7d6e707
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-05-12 04:22:15 +0300

    task1,2,3-done

---
+++ js/task1.js
@@ -0,0 +1,17 @@
+// =====================================Задание 1=====================================
+
+const delay = ms => {
+    const DELAY = ms;
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, DELAY);
+    })
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+
+// delay(2000).then(logger);
+// delay(1000).then(logger);
+// delay(1500).then(logger);
